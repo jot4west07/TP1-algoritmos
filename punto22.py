27229ec1811f83bf52b10e90c9c0fed32bacdfa1
Rome10 <souter3a@gmail.com>
2024-11-12 08:09:27 -0300

    Se agregan comentarios al codigo

--- punto22.py
+++ punto22.py
@@ -1,20 +1,20 @@
 def usar_la_fuerza(mochila, indice=0):
     if indice >= len(mochila):
         # No quedan más objetos en la mochila
-        return -1
+        return -1 # la función devuelve -1 para indicar que no se encontró el objeto en la mochila
     
     objeto_actual = mochila[indice]
     if objeto_actual == "sable de luz":
         # Encontramos un sable de luz
-        return indice
+        return indice # indice es el objeto actual en la mochila
     else:
         # Seguimos buscando
-        return usar_la_fuerza(mochila, indice + 1)
+        return usar_la_fuerza(mochila, indice + 1) # indice+1 para pasar al siguiente objeto 
 
 # Ejemplo de uso
 mochila_ejemplo = ["comida", "agua", "mapa", "sable de luz"]
 resultado = usar_la_fuerza(mochila_ejemplo)
-if resultado != -1:
+if resultado != -1: # si resultado es distinto a -1 se encotro el sables
     print(f"Encontramos un sable de luz después de sacar {resultado} objetos.")
 else:
     print("No se encontró un sable de luz en la mochila.")
--- punto5.py
+++ punto5.py
@@ -2,10 +2,15 @@ def romano_entero(romano):
     if not romano:
         return 0 
     romanos = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    # romano[0]: obtiene el primer caracter del string romano
+    # romanos[romano[0]]: Utiliza ese primer carácter para acceder al valor correspondiente en el diccionario romanos. En este caso, si el primer carácter es 'I', el valor en el diccionario romanos es 1 (ya que 'I': 1 en el diccionario)
+    # romano_entero(romano[1:]) llama a la función romano_entero, pero esta vez pasa la subcadena sin el primer caracter. En lugar de procesar todo el string, ahora solo se esta procesando el resto de los caracteres
+    # Finalmente, se hace una suma entre el valor del primer carácter y el resultado de la llamada recursiva romano_entero(romano[1:]), hasta la letra elegida
     return romanos[romano[0]] + romano_entero(romano[1:])
 
 print("Ingresar numero romano")
-romano = input()
+ingreso = input()
+romano = ingreso.upper() # Pasar a maysuculas porque la lista de romanos esta en mayusculas
 print("El numero romano convertido a decimal es: ")
 print(romano_entero(romano))
 
